62b368fb024ec137584a319c30db72d251ad0702
Mitos Kalandiel <mitoskalandiel@gmail.com>
2016-12-21 17:03:01 +0000

    some JavaFX additions  - added a mainmenu  - added a close event  - linked close menu to close event Signed-off-by:Mitos Kalandiel <mitoskalandiel@gmail.com>

--- src/main/java/me/mitoskalandiel/hostsedit/FXMLController.java
+++ src/main/java/me/mitoskalandiel/hostsedit/FXMLController.java
@@ -9,20 +9,29 @@ import javafx.scene.control.Label;
 import me.mitoskalandiel.jokerlibs.*;
 
 public class FXMLController implements Initializable {
-    
+
+    private final boolean doDebug = true;
+
     @FXML
     private Label label;
-    
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         String pw;
         pw = new String(JokerCrypto.generatePassword(10));
-        System.out.println("You clicked me!");
+        if (doDebug) {
+            System.out.println("You clicked me!");
+        }
         label.setText("Hello World! :: " + pw);
     }
-    
+
+    @FXML
+    private void handleClose(ActionEvent event) {
+        System.exit(0);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 }
